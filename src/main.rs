bfed175dbffd43e1a9049c325f6a0acaf905a754
Patrick Bell <zclpatrick@gmail.com>
2023-06-19 10:20:25 +1000

    refactored voxelization, added z flip so it now gives the correct result

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ mod mesh;
 use math::*;
 use voxel::*;
 use std::{fs::OpenOptions, process};
-use ordered_float::*;
 
 use crate::fast_solver::*;
 use crate::mesh::*;
@@ -39,79 +38,16 @@ fn main() {
 
     // dimensions in meters
     let dim_u = v3(1.75, 2.5, 1.5);
-    let dim_m = dim_u * U_TO_M;
-    let voxel_s = 0.01;
-    let voxel_su = voxel_s / U_TO_M;
-    let dim_vox = ((dim_m.x / voxel_s) as usize, (dim_m.y / voxel_s) as usize, (dim_m.z / voxel_s) as usize);
 
-    dbg!(&dim_vox);
-
-    let mut voxels = vec![0; dim_vox.0*dim_vox.1*dim_vox.2];
-
-    let z_triangles: Vec<Tri> = mesh.tris.iter().filter(|tri| tri.normal.dot(v3(0.0, 0.0, 1.0)) != 0.0).map(|x| *x).collect();
-
-    for i in 0..dim_vox.0 {
-        for j in 0..dim_vox.1 {
-            // ray intersections down column
-            let ray_z_offset = 0.1;
-            let ray_origin = v3((i as f32 + 0.5) / dim_vox.0 as f32 * dim_u.x, (j as f32 + 0.5) / dim_vox.1 as f32 * dim_u.y, dim_u.z + ray_z_offset);
-            // there was a copy paste error here but i thought it was working
-            // now its fixed and saying one of endpoints is at y=1700
-            let ray_dir = v3(0.0, 0.0, -1.0);
-            let mut intersections = Vec::new();
-            for tri in z_triangles.iter() {
-                let v0 = mesh.verts[tri.i1];
-                let v1 = mesh.verts[tri.i2];
-                let v2 = mesh.verts[tri.i3];
-                if let Some(d_inter) = ray_triangle_intersection(ray_origin, ray_dir, v0, v1, v2) {
-                    intersections.push(OrderedFloat(d_inter - ray_z_offset));
-                }
-            }
-            intersections.sort();
-
-            if i == 0 && j == 0 {
-                dbg!(intersections.clone());
-            }
-
-            // now make the voxels themselves
-            // project from the top and if passing through an odd number of triangles, empty otherwise filled. (Normally it would be the reverse but this volume denotes empty space)
-            // hoping the ceiling case is handled
-            let mut n_intersections = 0;
-            for k in 0..dim_vox.2 {
-                let vx_d = k as f32 * voxel_su;
-                loop {
-                    if n_intersections >= intersections.len() {
-                        break;
-                    }
-                    if vx_d >= intersections[n_intersections].0 {
-                        n_intersections += 1;
-                    } else {
-                        break;
-                    }
-                }
-                if n_intersections % 2 == 1 {
-                    voxels[i*dim_vox.1*dim_vox.2 + j*dim_vox.2 + k] = 0;
-                } else {
-                    voxels[i*dim_vox.1*dim_vox.2 + j*dim_vox.2 + k] = 1;
-                }
-            }
-        }
-    }
+    let voxels = Voxels::from_mesh(&mesh, dim_u);
+    let dim_vox = voxels.dim;
+    let voxels = voxels.voxels;
 
     // how do I check the correctness of the voxels lmao. export to minecraft.
-
-    println!("done");
-    let nfull = voxels.iter().filter(|v| **v == 1).count();
-    let nempty = voxels.iter().filter(|v| **v == 0).count();
-
-    println!("nfull {} nempty {} ratio {}", nfull, nempty, nfull as f32 / (nfull + nempty) as f32);
-
-    // well thats what I'd expect so I guess its working
 
     // what if we add slightly along normal to coord
     // doesnt really matter anyway
     // let hole_voxel_coords: Vec<(usize, usize, usize)> = endpoints.iter().map(|x| x.pos + 0.001*x.normal).map(|v| (((v.x / dim_u.x) * vx_x as f32) as usize, ((v.y / dim_u.y) * vx_y as f32) as usize, ((v.z / dim_u.z) * vx_z as f32) as usize)).collect();
-
 
     // lets print out the endpoints
     dbg!(&dim_u);
@@ -171,6 +107,9 @@ fn main() {
     // }
     // dbg!("done");
 
+    let voxel_mesh = gen_wall_mesh(&voxels, dim_vox, dim_u);
+    voxel_mesh.save("voxmesh.stl");
+
     let mut solver = FastSolver::new(dim_vox, voxel_endpoints, voxels);
     dbg!("begin");
     solver.solve_from(0);
@@ -196,3 +135,6 @@ fn main() {
 // the discrepancy might be from the mesh-> voxels step
 // I could output a voxel mesh, probably go for a low res one tho otherwise itll be utterly cooked
 // and probably should neighbour check to omit quads as well
+// maybe just z is flipped or something
+// but flip z fails at something 1
+// edges n shiz
--- src/voxel.rs
+++ src/voxel.rs
@@ -1,5 +1,9 @@
+use crate::mesh::*;
+use crate::math::*;
+use ordered_float::*;
+
 pub const U_TO_M: f32 = 3.6429696;
-pub const VOXEL_S_M: f32 = 0.01;
+pub const VOXEL_S_M: f32 = 0.1;
 pub const VOXEL_S_U: f32 = VOXEL_S_M / U_TO_M;
 
 pub fn vox_ind(pos: (usize, usize, usize), dim: (usize, usize, usize)) -> usize {
@@ -12,4 +16,78 @@ pub fn vox_indi(pos: (isize, isize, isize), dim: (usize, usize, usize)) -> usize
 pub fn vox_ind_ok(pos: (isize, isize, isize), dim: (usize, usize, usize)) -> bool {
     pos.0 >= 0 && pos.1 >= 0 && pos.2 >= 0 &&
     (pos.0 as usize) < dim.0 && (pos.1 as usize) < dim.1 && (pos.2 as usize) < dim.2
+}
+pub fn vox_indi_option(pos: (isize, isize, isize), dim: (usize, usize, usize)) -> Option<usize> {
+    if vox_ind_ok(pos, dim) {
+        Some(vox_indi(pos, dim))
+    } else {
+        None
+    }
+}
+
+pub struct Voxels {
+    pub voxels: Vec<usize>,
+    pub dim: (usize, usize, usize),
+}
+
+impl Voxels {
+    pub fn from_mesh(mesh: &IndexedMesh, dim_u: V3) -> Self {
+        let dim_m = dim_u * U_TO_M;
+        let dim_vox: (usize, usize, usize) = ((dim_m.x / VOXEL_S_M) as usize, (dim_m.y / VOXEL_S_M) as usize, (dim_m.z / VOXEL_S_M) as usize);
+
+        let mut voxels = vec![0; dim_vox.0*dim_vox.1*dim_vox.2];
+
+        let z_triangles: Vec<Tri> = mesh.tris.iter().filter(|tri| tri.normal.dot(v3(0.0, 0.0, 1.0)) != 0.0).map(|x| *x).collect();
+    
+        for i in 0..dim_vox.0 {
+            for j in 0..dim_vox.1 {
+                // ray intersections down column
+                let ray_z_offset = 0.1;
+                let ray_origin = v3((i as f32 + 0.5) / dim_vox.0 as f32 * dim_u.x, (j as f32 + 0.5) / dim_vox.1 as f32 * dim_u.y, dim_u.z + ray_z_offset);
+                // there was a copy paste error here but i thought it was working
+                // now its fixed and saying one of endpoints is at y=1700
+                let ray_dir = v3(0.0, 0.0, -1.0);
+                let mut intersections = Vec::new();
+                for tri in z_triangles.iter() {
+                    let v0 = mesh.verts[tri.i1];
+                    let v1 = mesh.verts[tri.i2];
+                    let v2 = mesh.verts[tri.i3];
+                    if let Some(d_inter) = ray_triangle_intersection(ray_origin, ray_dir, v0, v1, v2) {
+                        intersections.push(OrderedFloat(d_inter - ray_z_offset));
+                    }
+                }
+                intersections.sort();
+    
+                if i == 0 && j == 0 {
+                    dbg!(intersections.clone());
+                }
+    
+                // now make the voxels themselves
+                // project from the top and if passing through an odd number of triangles, empty otherwise filled. (Normally it would be the reverse but this volume denotes empty space)
+                // hoping the ceiling case is handled
+                let mut n_intersections = 0;
+                for k in 0..dim_vox.2 {
+                    let vx_d = k as f32 * VOXEL_S_U;
+                    loop {
+                        if n_intersections >= intersections.len() {
+                            break;
+                        }
+                        if vx_d >= intersections[n_intersections].0 {
+                            n_intersections += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    let k_flip = dim_vox.2 - k - 1;
+                    if n_intersections % 2 == 1 {
+                        voxels[i*dim_vox.1*dim_vox.2 + j*dim_vox.2 + k_flip] = 0;
+                    } else {
+                        voxels[i*dim_vox.1*dim_vox.2 + j*dim_vox.2 + k_flip] = 1;
+                    }
+                }
+            }
+        }
+
+        Voxels { voxels, dim: dim_vox }
+    }
 }
